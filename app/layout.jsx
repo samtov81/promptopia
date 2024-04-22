b92a1efb4999f9ced1232319de98ff15bb96ea4d
Samuel Tovar <sato.wd@gmail.com>
2024-04-22 11:45:41 -0400

    Google oauth provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,17 +7,21 @@ export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts"
 }
+
+
 const RootLayout = ({children}) => {
   return (
     <html lang='en'>
       <body>
-        <div className='main'>
-          <div className='gradient' />
-        </div>
-        <main className='app'>
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className='main'>
+            <div className='gradient' />
+          </div>
+          <main className='app'>
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   )
